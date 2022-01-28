45c85ca475dd6226ba3292c9dc72f9cccd913442
kzmgit <qiandaoyisui@gmail.com>
2022-01-28 10:02:00 +0000

    [Add] ユーザー編集機能を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+
   def index
     @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   def new
@@ -19,16 +26,39 @@ class UsersController < ApplicationController
     end
   end
 
-  def show
+  def edit
     @user = User.find(params[:id])
   end
 
-  def edit
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Profile Update!"
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
   end
 
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    # beforeアクション
+    # ログイン済みユーザーかどうか確認
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "ログインしてください"
+        redirect_to login_path
+      end
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      unless current_user?(@user)
+        redirect_to root_path
+      end
     end
 end
