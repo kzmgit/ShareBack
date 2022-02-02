11befd9757c0a9eb54ff3f1a97c5abcc52910ee7
kzmgit <qiandaoyisui@gmail.com>
2022-02-02 13:15:06 +0000

    [Add] ユーザー削除機能を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
-  before_action :correct_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy]
 
   def index
     @users = User.all
@@ -16,7 +16,6 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    # debugger
     if @user.save
       flash[:success] = "Welcome to the ShareBack!"
       log_in(@user)
@@ -40,6 +39,17 @@ class UsersController < ApplicationController
     end
   end
 
+    def destroy
+      @user = User.find(params[:id])
+      if @user.destroy
+        flash[:success] = "アカウントを削除しました"
+        redirect_to root_path
+      else
+        render :edit
+      end
+    end
+
+
   private
 
     def user_params
@@ -54,7 +64,7 @@ class UsersController < ApplicationController
         redirect_to login_path
       end
     end
-    
+
     def correct_user
       @user = User.find(params[:id])
       unless current_user?(@user)
