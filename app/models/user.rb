e61a9d3c70d3d1a327ea3ce533f40535cc16dfbe
kzmgit <qiandaoyisui@gmail.com>
2022-02-08 08:42:55 +0000

    [Add]モデルとコントローラーを作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
-  has_many :products
+  has_many :products, dependent: :destroy
+  has_many :feedbacks, dependent: :destroy
   has_one_attached :image
 
   validates :name,  presence: true
