01a4f4edcbb550e6557da1dfe72cdd4905f964d3
kzmgit <qiandaoyisui@gmail.com>
2022-02-12 08:33:26 +0000

    [Update] productsテーブルにカラムを追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,14 @@
 class Product < ApplicationRecord
   belongs_to :user
   has_many :feedbacks, dependent: :destroy
+  has_one_attached :image
 
-  validates :title,  presence: true
-  validates :body,  presence: true
+  with_options presence: true do
+  validates :title
+  validates :overview
+  validates :body
+  validates :features
+  validates :technology
+  end
+
 end
--- db/migrate/20220204080908_create_products.rb
+++ db/migrate/20220204080908_create_products.rb
@@ -3,8 +3,11 @@ class CreateProducts < ActiveRecord::Migration[6.1]
     create_table :products do |t|
       t.references :user, null: false, foreign_key: true
       t.string :title, null: false
+      t.string :overview, null: false
       t.text :body, null: false
-
+      t.text :features, null: false
+      t.text :technology, null: false
+      
       t.timestamps
     end
   end
