799e0643d351f5df13fa5a1cbd9021e6078d73bc
kzmgit <qiandaoyisui@gmail.com>
2022-02-17 09:59:12 +0000

    [Update]get_imageメソッドを作成して画像呼び出しを簡潔にした。

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,15 @@ class Product < ApplicationRecord
   has_many :feedbacks, dependent: :destroy
   has_one_attached :image
 
+  # 画像が存在しない場合にデフォルト画像をActiveStorageに格納する&サイズ調整。
+  def get_image(width, height)
+  unless image.attached?
+    file_path = Rails.root.join('app/assets/images/no_app_image.jpeg')
+    image.attach(io: File.open(file_path), filename: 'default-image.jpeg', content_type: 'image/jpeg')
+  end
+  image.variant(resize_to_limit: [width, height]).processed
+  end
+
   with_options presence: true do
   validates :title
   validates :url
